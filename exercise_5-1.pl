effc0dcdf444defc25ee2c95126072f722548242
Julius Gonsior <julius@gaenseri.ch>
2015-10-15 11:50:18 +0200

    5.1: checkPlan works

--- exercise_5-1.pl
+++ exercise_5-1.pl
@@ -12,11 +12,9 @@ container(d,1,1).
 on(a,d).
 on(b,c).
 on(c,d).
-%on(d,e).
-%on(e,f).
-%on(e,g).
-% it is possible to unload A -> works only if there is no other container on it
-unload(a).
+on(d,e).
+on(e,f).
+on(e,g).
 
 % plan(HiredWorkers,TotalDuration, TotalCost)
 plan(HiredWorkers, TotalDuration, Totalcost).
@@ -27,33 +25,30 @@ unload([], [[]]).
 unload([Container|ContainerList], [[Container|CurrentUnloading]|OtherUnloadings]) :-
 	checkIfOtherContainersUnloaded(Container, OtherUnloadings).
 
-% checkIfOtherContainersUnloaded(Container, OtherUnloadings)
-% checkIfOtherContainersUnloaded(a, [[b,c],[e]).
+
+
+calculate(ContainerList,Plan,Price,Workers) :-
+	checkPlan(ContainerList, Plan).
+
+% checkPlan(Plan) -> needs to check if all containers from on() are being used
+% then needs to check if it is allowed to unload the containers that are in this plan --> the plan contains all the timestamps!
+% all container from the first unloading are only those, that have nothing on them
+% all container from the second unloading are only those, that have nothing on them or are unloaded in the first round
+checkPlan([], _).
+
+checkPlan([OneUnloading|OtherUnloadings], ContainersThatCanBeUsed) :-
+	checkIfOtherContainersUnloaded(OneUnloading, ContainersThatCanBeUsed),
+	append(OneUnloading, ContainersThatCanBeUsed, ContainersThatCanBeUsedNextTime),
+	checkPlan(OtherUnloadings, ContainersThatCanBeUsedNextTime).
+
+
+% checkIfOtherContainersUnloaded(ContainerList, ContainersThatCanBeUsed)
+% checkIfOtherContainersUnloaded([a], [b,c,e).
 % so all containers that are on top of a need to be in otherunloadings
 %
-checkIfOtherContainersUnloaded(Container, UnloadPlan) :-
+checkIfOtherContainersUnloaded([Container|ContainerList], UnloadPlan) :-
 	onTop(Container, TopContainers),
-	contained(TopContainers, UnloadPlan).
-
-checkIfOtherContainersUnloaded(Container, [FirstList|OtherUnloadings]) :-
-	checkIfOtherContainersUnloaded(Container, OtherUnloadings).
-
-% contained(ContainerList, UnloadPlan) 
-% checks if every Container from ContainerList is contained in the UnloadPlan
-contained([],_).
-contained([Container|ContainerListRest], [Unloading|UnloadPlanRest]) :-
-	member(Container, Unloading),
-	contained(ContainerListRest, [Unloading|UnloadPlanRest]).
-
-contained([Container|ContainerListRest], [Unloading|UnloadPlanRest]) :-
-	contained([Container], UnloadPlanRest),
-	contained(ContainerListRest, [Unloading|UnloadPlanRest]).
-
-
-
-
-
-
+	sublist(TopContainers, UnloadPlan).
 
 %BUG -> result is being returned twice!
 % onTop(Container, OnTopContainerList) -> lists all containers that are on top of this container
@@ -72,15 +67,8 @@ onTop2([Container|TopContainerRest], Result) :-
 	onTop(Container,Result).
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+% sublist(List1, List2) true if every element from List1 is contained in List2
+sublist([], _).
+sublist([X|Xs], List2) :-
+	member(X, List2),
+	sublist(Xs, List2).
